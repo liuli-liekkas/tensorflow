76a1be17a2bbade0fc6da2cdaa2cfe2f8bc4f276
unknown <lil1@svic.com>
2019-01-29 13:39:35 +0800

    Initial commit

---
+++ number_test.py
@@ -0,0 +1,18 @@
+"""Functions for downloading and reading MNIST data."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+# pylint: disable=unused-import
+import gzip
+import os
+import tempfile
+
+import numpy
+from six.moves import urllib
+from six.moves import xrange # pylint: disable=redefined-builtin
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets # pylint: enable=unused-import
+import tensorflow.examples.tutorials.mnist.input_data as input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
