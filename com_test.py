d6d4039b64c27bc90178469dd00cbe76539be663
liuli <ll382640782@126.com>
2019-03-09 19:25:18 +0800

    edit

--- com_test.py
+++ com_test.py
@@ -6,6 +6,8 @@ import numpy as np
 import tkinter
 from tkinter import scrolledtext
 from tkinter import ttk
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 from threading import Thread
 import _tkinter
 
@@ -83,6 +85,17 @@ class Application:
         scr_show.see('end')
         return line
 
+    def paint(self, x_data, y_data, x_label, y_label, title, name):
+        plt.figure()
+        plt.xlabel(x_label, size=14)
+        plt.ylabel(y_label, size=14)
+        plt.title(title, size=14)
+        plt.plot(x_data, y_data, linewidth=2, c='b')
+        plt.grid(True)
+        plt.axis('tight')
+        plt.show()
+        plt.savefig(name + '.jpg')
+
     # 三点计算公式
     def geo_distance_three(self, lng_test, lat_test, h_test, lng_default, lat_default, h_default):
         lng_test, lat_test, lng_default, lat_default = map(math.radians, [lng_test, lat_test, lng_default, lat_default])
@@ -107,6 +120,7 @@ class Application:
         utc_time = False
         location = False
         data_test = []
+        time_start = 0
         while True:
             time.sleep(0.01)
             line = self.get_data()
@@ -136,6 +150,7 @@ class Application:
                     if line.split(',')[3]:
                         if not location:
                             time_delta = time.time() - time_start
+                            time_delta = str(time_delta)
                             scr_test.insert('end, '"系统开始定位,首次捕获时间为：" + time_delta)  # 计算捕获时间
                             scr_test.update()
                             scr_test.see('end')
@@ -157,106 +172,18 @@ class Application:
                             geo_distance = self.geo_distance_two(data_test[0], data_test[1], lat_default, lng_default)
                             if geo_distance > 100 or data_test[3] > 100:
                                 np.delete(data_test, 0, axis=1)
-                                scr_test.insert('end', '定位无效，误差为' + geo_distance + '米')
+                                geo_distance = str(geo_distance)
+                                scr_test.insert('end', "定位无效，水平误差为：" + geo_distance + '米'
+                                                + '\n' + '定位无效，垂直误差为：' + geo_distance + '米')
                                 scr_test.update()
                                 scr_test.see('end')
                             elif geo_distance < 100 and data_test[3] < 100:
-                                scr_test.insert('end', '定位有效，误差为' + geo_distance + '米')
+                                geo_distance = str(geo_distance)
+                                scr_test.insert('end', '定位有效，水平误差为：' + geo_distance + '米'
+                                                + '\n' + '定位无效，垂直误差为：' + geo_distance + '米')
                                 scr_test.update()
                                 scr_test.see('end')
                                 break
-
-class Clock:
-    def __init__(self, master, x, y, width, height, radius):
-        # :param master: 父窗口
-        # :param x: 时钟中心点的x坐标
-        # :param y: 时钟中心点的y坐标
-        # :param width: 画布的宽度
-        # :param height: 画布的高度
-        # :param radius: 时钟钟盘的半径
-        self.centerX = x
-        self.centerY = y
-        self.radius = radius
-        self.canvas = tkinter.Canvas(master, width=width, height=height) # 画布
-        self.canvas.pack()
-        self.canvas.create_oval(
-          x - radius,
-          y - radius,
-          x + radius,
-          y + radius) # 画钟框
-        self.id_lists = []
-        self.hourHandRadius = self.radius * 1.0 / 4  # 指针长度
-        self.minHandRadius = self.radius * 2.0 / 3  # 分针长度
-        self.secHandRadius = self.radius * 4.0 / 5  # 秒针长度
-        self.timeVar = tkinter.StringVar()
-        # self.timeVar.set('')
-        self.timeLabel = tkinter.Label(self.canvas.master, textvariable=self.timeVar)
-        self.timeLabel.pack(side=tkinter.BOTTOM)
-        #self.canvas.master.protocol('WM_DELETE_WINDOW', self.canvas.master.destroy)
-
-    def __del__(self):
-        self._deleteItems(self.id_lists)
-
-      # 绘制时钟钟盘
-    def drawClockDial(self):
-        # 绘制钟盘上的数字1-12
-        r = self.radius - 15
-        for i in range(1, 13):
-            rad = 2 * math.pi / 12 * i
-            x = self.centerX + math.sin(rad) * r
-            y = self.centerY - math.cos(rad) * r
-            id = self.canvas.create_text(x, y, text=str(i))
-            self.id_lists.append(id)
-        # 绘制钟盘上的刻度
-        r1 = self.radius - 5
-        r2 = self.radius
-        for i in range(1, 61):
-            rad = 2 * math.pi / 60 * i
-            x1, y1 = self._getPosByRadAndRadius(rad, r1)
-            x2, y2 = self._getPosByRadAndRadius(rad, r2)
-            id = self.canvas.create_line(x1, y1, x2, y2)
-            self.id_lists.append(id)
-        # 显示时间
-    def showTime(self, tm):
-        hour = tm.tm_hour % 12
-        min = tm.tm_min
-        sec = tm.tm_sec
-        sec_rad = 2 * math.pi / 60 * sec
-        min_rad = 2 * math.pi / 60 * (min + sec / 60.0)
-        hour_rad = 2 * math.pi / 12 * (hour + min / 60.0)
-        timeStr = '当前时间: %d-%02d-%02d %02d:%02d:%02d' % (tm.tm_year, tm.tm_mon, tm.tm_mday, hour, min, sec)
-        self.timeVar.set(timeStr)
-        hour_id = self._drawLine(hour_rad, self.hourHandRadius, 6)
-        min_id = self._drawLine(min_rad, self.minHandRadius, 4)
-        sec_id = self._drawLine(sec_rad, self.secHandRadius, 3)
-        return (hour_id, min_id, sec_id)
-    def run(self):
-        def _run():
-            while True:
-                tm = time.localtime()
-                id_lists = self.showTime(tm)
-                self.canvas.master.update()
-                time.sleep(1)
-                self._deleteItems(id_lists)
-                thrd = Thread(target=_run) # 创建新的线程
-                thrd.run() # 启动线程
-
-    def _drawLine(self, rad, radius, width):
-        x, y = self._getPosByRadAndRadius(rad, radius)
-        id = self.canvas.create_line(self.centerX, self.centerY, x, y, width=width)
-        return id
-
-    def _getPosByRadAndRadius(self, rad, radius):
-        x = self.centerX + radius * math.sin(rad)
-        y = self.centerY - radius * math.cos(rad)
-        return (x, y)
-
-    def _deleteItems(self, id_lists):
-        for id in id_lists:
-            try:
-                self.canvas.delete(id)
-            except BaseException:
-                pass
 
 
 # def get_data(self):
@@ -397,12 +324,3 @@ class Clock:
 
 if __name__ == '__main__':
     app = Application()
-
-
-
-
-
-
-
-
-
